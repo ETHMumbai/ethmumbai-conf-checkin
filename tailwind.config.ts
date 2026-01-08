4cadcfdbae9af86da70da9a892ad6b66d9c837fc
Diksha Srivastava <90517961+diksha-2911@users.noreply.github.com>
2026-01-08 20:09:29 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        mplus: ['MPlusRounded1c', 'sans-serif'],
+        sans: ["var(--font-inter)", "sans-serif"],
+      },
+    },
+  },
+};
